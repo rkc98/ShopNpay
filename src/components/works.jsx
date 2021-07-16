41fa1456bda6c7c237a11a10ae391b8ae0f18ff2
rkc98 <47149870+rkc98@users.noreply.github.com>
2021-07-16 16:55:42 +0530

    Update works.jsx

--- src/components/works.jsx
+++ src/components/works.jsx
@@ -11,14 +11,16 @@ const Works = () => {
     <h2 style={{textAlign:"center" , fontWeight:"bold", marginBottom:"40px"}}>How ShopNpay Works</h2>
     
     <div className="col-md-2" style={{textAlign:'center', fontWeight:"bold"}}>
+    <p style={{fontSize:"13px"}}>Register</p>
     <BsFillPersonLinesFill className="iconstyle" />
-    <p>Register</p>
+    <p>Register onboard</p>
     </div>
    
     <div className="col-md-1">
       <BsArrowRight className="arrowiconstyle"  />
     </div>
     <div className="col-md-2" style={{textAlign:'center', fontWeight:"bold"}}>
+      <p style={{fontSize:"13px"}}>Send payment link</p>
     <BsFillChatDotsFill className="iconstyle" />
     <p>Send payment link to the consumer</p>
     </div>
@@ -26,6 +28,7 @@ const Works = () => {
       <BsArrowRight className="arrowiconstyle" />
     </div>
     <div className="col-md-2" style={{textAlign:'center', fontWeight:"bold"}}>
+      <p style={{fontSize:"13px"}}>Multilingual payment mode</p>
     <BsCreditCard className="iconstyle" />
     <p>Receive your payments online through all payment modes</p>
     </div>
@@ -33,12 +36,13 @@ const Works = () => {
       <BsArrowRight className="arrowiconstyle" />
     </div>
     <div className="col-md-2" style={{textAlign:'center', fontWeight:"bold"}}>
+      <p style={{fontSize:"13px"}}>Payment dashboard</p>
       <BsFileEarmarkText className="iconstyle" />
       <p>View all your payment receipts in one place</p>
     </div>
     </div>
     </div>
-    
+
 
         </>
     )
